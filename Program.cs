c48df4a73ff5ce2dfe678cb7798bad9200f99247
Ilya <fominiv10@gmail.com>
2022-10-15 23:38:13 +0300

    edit metod

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк
 // длина которых меньше либо равна 3 символа.
 
-string [] array = {"корова", "24", "пирог", "на", "трава"};
+string [] array = {"корова", "24", "пирог", "на", "ПП", "трава"};
 
-string [] NewArray3letters (string [] array)
+void OutputNewArray3letters (string [] array)
 {
     int k = 0;
     for (int i = 0; i < array.Length; i++)
@@ -15,13 +15,19 @@ string [] NewArray3letters (string [] array)
     }
     int j = 0;
     string [] newArray = new string [k];
-    for (int i = 0; i < array.Length; i++)
+    for (int l = 0; l < array.Length; l++)
     {
-        if (array[i].Length < 4)
+        if (array[l].Length < 4)
         {
-            newArray[j] = array[i];
+            newArray[j] = array[l];
             j = j + 1;
         }
     }
-    return newArray;
-} 
+    Console.Write("newArray = [");
+    for (int n = 0; n < newArray.Length - 1; n++)
+    {
+        Console.Write($"{newArray[n]}, ");
+    }
+    Console.Write($"{newArray[newArray.Length - 1]}]");
+}
+-
