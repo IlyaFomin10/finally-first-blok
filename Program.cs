29251bd63eabf1c3c362574efec3c8b4ce35d3de
Ilya <fominiv10@gmail.com>
2022-10-15 20:30:32 +0300

    create metod

--- Program.cs
+++ Program.cs
@@ -3,3 +3,25 @@
 
 string [] array = {"корова", "24", "пирог", "на", "трава"};
 
+string [] NewArray3letters (string [] array)
+{
+    int k = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            k = k + 1;
+        }    
+    }
+    int j = 0;
+    string [] newArray = new string [k];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            newArray[j] = array[i];
+            j = j + 1;
+        }
+    }
+    return newArray;
+} 
